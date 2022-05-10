e2c507c904200667ec3121b16a021f6905df894c
Michael Lima <michael.cdl@outlook.com>
2022-05-10 23:26:29 +0200

    Scrollable Animation For Post

--- components/Home/FocusBlock/FocusBlock.js
+++ components/Home/FocusBlock/FocusBlock.js
@@ -1,28 +1,15 @@
-import React, {useState, useEffect} from 'react'
-import {View, Text, TouchableOpacity, FlatList} from 'react-native'
+import React, {useState, useRef} from 'react'
+import {View, Text, TouchableOpacity, FlatList, TextInput, Animated} from 'react-native'
 import Posts from './Posts'
-const DATA = [
-    {
-        id: 'asdjfnal;sdkjf',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        name: 'Michael'
-    },
-    {
-        id: 'asdflaksdf;sdkjf',
-        content: 'Lorem ipsum dolor sit amet',
-        name: 'Ariel'
-    },
-    {
-        id: 'asdfjkalsdjfnl;sdkjf',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad ',
-        name: 'Lima'
-    },
-]
 const FocusBlock = ({route, navigation}) => {
-
+    const [post, setPost] = useState('')
+    const [trackY, setY] = useState(-1)
+    const fadeAnim = useRef(new Animated.Value(150)).current;
+    const fadeOpa = useRef(new Animated.Value(1)).current;
     const goBack= () => {
         navigation.goBack()
     }
+    
 
     const rednderItem = ({item}) => {
         const data = {id:item.id, content:item.content, name:item.name}
@@ -30,7 +17,36 @@ const FocusBlock = ({route, navigation}) => {
             <Posts navigate={() => navigation.navigate('FocusPost', data)} id={item.id} content={item.content} name={item.name}/>
         )
     }
-
+    const handleScroll= (y) => {
+    
+        if(y < trackY - 20) {
+            setY(y)
+            console.log('up', y)
+            Animated.timing(fadeAnim, {
+                toValue: 0,
+                duration: 500
+              }).start();
+              Animated.timing(fadeOpa, {
+                toValue: 0,
+                duration: 500
+              }).start();
+        } 
+        if(y > trackY + 20) {
+            if(y < 50){
+                setY(y)
+                console.log('down', y)
+                Animated.timing(fadeAnim, {
+                    toValue: 150,
+                    duration: 500
+                }).start();
+                Animated.timing(fadeOpa, {
+                    toValue: 1,
+                    duration: 500
+                  }).start();
+            }
+        }
+        
+    }
     return(
         <View style={{backgroundColor: 'white'}}>
             <View style={{width: '100%', alignItems: 'center'}}>
@@ -38,9 +54,20 @@ const FocusBlock = ({route, navigation}) => {
                 <Text style={{fontSize: 20,margin: 2, marginTop: 10}}>{route.params.title}</Text>
                 <Text style={{marginBottom: 20}}>{route.params.admin}</Text>
             </View>
+            <Animated.View useNativeDriver={true} style={{borderWidth: 0.3, margin: 4, borderRadius: 20, padding: 3, height: fadeAnim, opacity: fadeOpa}}>
+                <TextInput
+                    style={{height: '70%', margin: 10}}
+                    onChange={setPost}
+                    value={post}
+                    multiline={true}
+                />
+                <TouchableOpacity alignItems='right' style={{position: 'absolute', right: 10, bottom: 10,backgroundColor: 'black',width:30, height: 30, borderRadius: 15}}>
+                </TouchableOpacity>
+            </Animated.View>
             <View style={{backgroundColor: '#7FFFD4', height: '100%'}}>
                <FlatList
                     data={DATA}
+                    onScroll={((e) => handleScroll(e.nativeEvent.contentOffset.y))}
                     renderItem={rednderItem}
                     keyExtractor={item => item.id}
                />
